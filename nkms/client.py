03ad17bf13e9689864695301aec7a62c97ddaad1
tuxxy <john@nucypher.com>
2017-11-05 17:44:19 -0800

    Add dummy methods for grant and revoke

--- nkms/client.py
+++ nkms/client.py
@@ -1,2 +1,15 @@
 class Client(object):
-    pass
+    def __init__(self):
+        pass
+
+    def grant(self):
+        """
+        Grants access to Bob to retrieve a key.
+        """
+        pass
+
+    def revoke(self):
+        """
+        Revokes access from Bob to retrieve a key.
+        """
+        pass
